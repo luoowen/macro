578104a91ef1a91e5104aba6024af917d2e344d3
prcao <patrickrcao@gmail.com>
2019-09-15 04:37:11 -0400

    fix exposed api key

--- Components/Home.js
+++ Components/Home.js
@@ -21,7 +21,7 @@ export default class Home extends React.Component {
         super(props);
 
         const firebaseConfig = {
-            apiKey: "AIzaSyAHRUXdaAgPoyu-gHnpqQZ2wC4aZfkPDvo",
+            apiKey: process.env.FIREBASE_API_KEY,
             authDomain: "macro-dbe61.firebaseapp.com",
             databaseURL: "https://macro-dbe61.firebaseio.com",
             projectId: "macro-dbe61",
